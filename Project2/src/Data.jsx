e185ee6bdd45d978d3047e85340eaa0f9c87b355
Roshan Gorakhpuriya <roshangorakhpuriya@gmail.com>
2024-05-10 23:12:19 +0100

    Second React Project

---
+++ Project2/src/Data.jsx
@@ -0,0 +1,53 @@
+const Data = [
+    {
+        id : 1,
+        name : "Agra",
+        info : "Agra is a city in the northern state of Uttar Pradesh, India. The city is famous for being the capital of the Mughal emperors from 1526 to 1658. It is a major tourist destination for its many Mughal-era buildings such as Tāj Mahal, Agra Fort and Fatehpūr Sikrī, all three of which are UNESCO World Heritage Sites.Agra is on the Indo-Gangetic plain and has a continental climate, with long, hot summers from April to September.",
+        image : "https://cdn.thecodehelp.in/Agra.jpeg",
+        price : "34,999"
+    },
+    {
+        id : 2,
+        name : "Jaipur",
+        info : "Jaipur, city, capital of Rajasthan state, northwestern India. It is situated in the east-central part of the state, roughly equidistant from Alwar (northeast) and AjmeA walled town surrounded (except to the south) by hills, the city was founded in 1727 by Maharaja Sawai Jai Singh to replace nearby Amber (now Amer, a part of Jaipur) as the capital of the princely state of Jaipur (founded by the Rajputs in the 12th century CE).",
+        image : "https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price : "79,999"
+    },
+    {
+        id : 3,
+        name : "Goa",
+        info : "Goa, state of India, comprising a mainland district on the country’s southwestern coast and an offshore island. It is located about 250 miles (400 km) south of Mumbai (Bombay). One of India’s smallest states, it is bounded by the states of Maharashtra on the north and Karnataka on the east and south and by the Arabian Sea on the west.",
+        image : "https://cdn.thecodehelp.in/Goa.jpeg",
+        price : "39,999"
+    },
+    {
+        id : 4,
+        name : "Varanasi",
+        info : "Varanasi is one of the oldest continuously inhabited cities in the world. It was one of the first major urban settlements in the middle Ganges valley. By the 2nd millennium BCE Varanasi was a seat of Vedic religion and philosophy and was also a commercial and industrial centre famous for its muslin and silk fabrics, perfumes, ivory works, and sculpture.",
+        image : "https://cdn.thecodehelp.in/Varanasi.jpeg",
+        price : "24,999"
+    },
+    {
+        id : 5,
+        name : " Darjeeling",
+        info : "Darjeeling, city, extreme northern West Bengal state, northeastern India. It lies about 305 miles (490 km) north of Kolkata, at an elevation of about 7,000 feet (2,100 metres) above sea level.Darjeeling is situated on a long, narrow mountain ridge of the Sikkim Himalayas that descends abruptly to the bed of the Great Rangit River.",
+        image : "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+        price : "45,999"
+    },
+    {
+        id : 6,
+        name : "Jaisalmer",
+        info : "The origin of Jaisalmer city dates back to the 12th century when a Bhati Rajput King named Rao Jaisala decided to shift his capital from Ludharva to a safer place. Thus the birth of Jaisalmer city in the year 1156 AD. In 1949, the city was included in the state of Rajasthan and granted the status of a district.",
+        image : "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+        price : "39,999"
+    },
+    {
+        id : 7,
+        name : "Kochi",
+        info : "Kochi, city and major port on the Malabar Coast of the Arabian Sea, west-central Kerala state, southwestern India. Also the name of a former princely state, “Kochi” is sometimes used to refer to a cluster of islands and towns, including Ernakulam, Mattancheri, Fort Cochin, Willingdon Island, Vypin Island, and Gundu Island.",
+        image : "https://cdn.thecodehelp.in/Kochi.jpeg",
+        price : "34,999"
+    },
+]
+
+ export default Data;
